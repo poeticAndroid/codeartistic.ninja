03cc44ac78cacdcd547c36b07af561c8395ad4f1
poeticAndroid <i@poeticAndroid.online>
2024-10-30 22:28:04 +0100

    more fleshed out game_entry

--- scenes/game_entry.gd
+++ scenes/game_entry.gd
@@ -1,13 +1,37 @@
+@tool
 extends Control
 
+@export var game_cover: Texture2D
+@export var game_title: String
+@export var game_name: String
+@export var game_description: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var node = $Node2D/Button
-	node.pivot_offset = node.size / 2
-	node.rotation += randf_range(-PI / 90, PI / 90)
+	var btn: Button = %Button
+	if game_cover: btn.icon  = game_cover
+	if game_title: btn.text = game_title
+	if game_description: btn.tooltip_text = game_description
+	btn.rotation = randf_range(-PI / 90, PI / 90)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	print("Starting game ", game_name)
+
+
+func _on_focus_entered() -> void:
+	%Button.grab_focus()
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+	tween.tween_property(%Button, "rotation", randf_range(-PI / 90, PI / 90), 1)
+	$AnimationPlayer.play("focus")
+	#%Button.rotation = randf_range(-PI / 90, PI / 90)
+
+
+func _on_focus_exited() -> void:
+	$AnimationPlayer.play_backwards("focus")
