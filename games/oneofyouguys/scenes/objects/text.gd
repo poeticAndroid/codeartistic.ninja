908a4e34367224ef97d9453e7b13c3dec11b0d3e
poeticAndroid <i@poeticAndroid.online>
2025-01-12 21:45:10 +0100

    text offset and stuff

--- games/oneofyouguys/scenes/objects/text.gd
+++ games/oneofyouguys/scenes/objects/text.gd
@@ -1,25 +1,27 @@
 @tool
-extends Label
+extends Node2D
 
 @export var cols: int = 24
 @export var center: bool = false
-@export var desktop_txt: String
-@export var touch_txt: String
-@export var gamepad_txt: String
+@export var text: String = "[txt]"
+@export var desktop_text: String
+@export var touch_text: String
+@export var gamepad_text: String
 
 var input_method: int = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if cols > 0:
-		size.x = label_settings.font_size * cols
+		$Label.size.x = $Label.label_settings.font_size * cols
+	$Label.text = text
 	var txt = JSON.parse_string(text)
 	if typeof(txt) == TYPE_STRING:
-		text = txt.replace("\n", " \n")
+		text = txt.replace("\n", "\n ")
 	if typeof(txt) == TYPE_ARRAY:
-		desktop_txt = txt[Global.DESKTOP_INPUT]
-		touch_txt = txt[Global.TOUCH_INPUT]
-		gamepad_txt = txt[Global.GAMEPAD_INPUT]
+		desktop_text = txt[Global.DESKTOP_INPUT].replace("\n", "\n ")
+		touch_text = txt[Global.TOUCH_INPUT].replace("\n", "\n ")
+		gamepad_text = txt[Global.GAMEPAD_INPUT].replace("\n", "\n ")
 		text = "[ctrl]"
 
 
@@ -30,8 +32,8 @@ func _process(delta: float) -> void:
 	input_method = Global.input_method
 	match input_method:
 		Global.DESKTOP_INPUT:
-			text = desktop_txt if desktop_txt else text
+			$Label.text = desktop_text if desktop_text else text
 		Global.TOUCH_INPUT:
-			text = touch_txt if touch_txt else text
+			$Label.text = touch_text if touch_text else text
 		Global.GAMEPAD_INPUT:
-			text = gamepad_txt if gamepad_txt else text
+			$Label.text = gamepad_text if gamepad_text else text
