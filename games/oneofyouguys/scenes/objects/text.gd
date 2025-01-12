5b0f117c3e6e85a86b68796dff8eff8f7ca3cdef
poeticAndroid <i@poeticAndroid.online>
2025-01-12 22:39:02 +0100

    property setters

--- games/oneofyouguys/scenes/objects/text.gd
+++ games/oneofyouguys/scenes/objects/text.gd
@@ -1,29 +1,35 @@
 @tool
 extends Node2D
 
-@export var cols: int = 24
+@export var cols: int = 24:
+	set(_cols):
+		cols = _cols
+		$Label.size.x = $Label.label_settings.font_size * cols
 @export var center: bool = false
-@export var text: String = "[txt]"
-@export var desktop_text: String
-@export var touch_text: String
-@export var gamepad_text: String
+@export_multiline var text: String = "[txt]":
+	set(_text):
+		text = _text
+		$Label.text = text
+
+		var txt = JSON.parse_string(text)
+		if typeof(txt) == TYPE_STRING:
+			text = txt.replace("\n", "\n ")
+		if typeof(txt) == TYPE_ARRAY:
+			desktop_text = txt[Global.DESKTOP_INPUT].replace("\n", "\n ")
+			touch_text = txt[Global.TOUCH_INPUT].replace("\n", "\n ")
+			gamepad_text = txt[Global.GAMEPAD_INPUT].replace("\n", "\n ")
+			text = "[ctrl]"
+
+@export_group("Control hints")
+@export_multiline var desktop_text: String
+@export_multiline var touch_text: String
+@export_multiline var gamepad_text: String
 
 var input_method: int = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if cols > 0:
-		$Label.size.x = $Label.label_settings.font_size * cols
-	$Label.text = text
-	var txt = JSON.parse_string(text)
-	if typeof(txt) == TYPE_STRING:
-		text = txt.replace("\n", "\n ")
-	if typeof(txt) == TYPE_ARRAY:
-		desktop_text = txt[Global.DESKTOP_INPUT].replace("\n", "\n ")
-		touch_text = txt[Global.TOUCH_INPUT].replace("\n", "\n ")
-		gamepad_text = txt[Global.GAMEPAD_INPUT].replace("\n", "\n ")
-		text = "[ctrl]"
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
