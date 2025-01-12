b552508d486510e1ee51b0ae30a6960e5c71c472
poeticAndroid <i@poeticAndroid.online>
2025-01-12 23:32:24 +0100

    clan colors

--- games/oneofyouguys/scenes/objects/body.gd
+++ games/oneofyouguys/scenes/objects/body.gd
@@ -1,13 +1,29 @@
+@tool
 extends Node2D
 
-@export var clan: String
+@export var clan: Clan:
+	set(_clan):
+		clan = _clan
+		match clan:
+			Clan.RANDOM:
+				$AnimatedSprite2D.modulate = Color.WHITE
+			Clan.ORANGE:
+				$AnimatedSprite2D.modulate = Color("FF9200")
+			Clan.GREEN:
+				$AnimatedSprite2D.modulate = Color("74FF00")
+			Clan.PURPLE:
+				$AnimatedSprite2D.modulate = Color("6E01FF")
 @export var possessed: bool
+
+enum Clan {RANDOM, ORANGE, GREEN, PURPLE}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
+	if Engine.is_editor_hint(): return
+	if clan == Clan.RANDOM:
+		clan = randi_range(1, 3)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Engine.is_editor_hint(): return
--- games/oneofyouguys/scenes/objects/door.gd
+++ games/oneofyouguys/scenes/objects/door.gd
@@ -1,12 +1,37 @@
+@tool
 extends Node2D
 
-@export var clan: String
+@onready var color_rect: ColorRect = $ColorRect
+
+@export var clan: Clan:
+	set(_clan):
+		clan = _clan
+		if not color_rect:return
+		match clan:
+			Clan.RANDOM:
+				color_rect.modulate = Color.WHITE
+			Clan.ORANGE:
+				color_rect.modulate = Color("FF9200")
+			Clan.GREEN:
+				color_rect.modulate = Color("74FF00")
+			Clan.PURPLE:
+				color_rect.modulate = Color("6E01FF")
+@export var size: Vector2 = Vector2.ONE:
+	set(_size):
+		size = _size
+		if not color_rect:return
+		color_rect.size = size
+		color_rect.position = size * -0.5
+
+enum Clan {RANDOM, ORANGE, GREEN, PURPLE}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
+	if Engine.is_editor_hint(): return
+	if clan == Clan.RANDOM:
+		clan = randi_range(1, 3)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Engine.is_editor_hint(): return
