3eb0f833d17f67f5322a2d419e93a815bc8c8d98
poeticAndroid <i@poeticAndroid.online>
2025-01-29 14:09:36 +0100

    the goal works

--- games/oneofyouguys/scenes/objects/scripts/goal.gd
+++ games/oneofyouguys/scenes/objects/scripts/goal.gd
@@ -1,7 +1,17 @@
+@tool
 @icon("./icons/flag_blue.png")
 extends Node2D
 
 @export var destination: String
+@export var size: Vector2 = Vector2.ONE:
+	set(_size):
+		size = _size
+		if not %ColorRect: await ready
+		%ColorRect.size = size
+		%ColorRect.position = size * -0.5
+		if not $CollisionShape2D: await ready
+		$CollisionShape2D.shape = RectangleShape2D.new()
+		$CollisionShape2D.shape.size = size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +21,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Body) -> void:
+	if not body.possessed: return
+	Global.replace_scene(destination)
