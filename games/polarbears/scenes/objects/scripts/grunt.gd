1d02b14c1df4e2c195e0383f5f14f0b6ff62ba34
poeticAndroid <i@poeticAndroid.online>
2025-02-23 15:12:52 +0100

    grunt movement

--- games/polarbears/scenes/objects/scripts/grunt.gd
+++ games/polarbears/scenes/objects/scripts/grunt.gd
@@ -1,6 +1,10 @@
 @icon("./icons/grunt.png")
 extends Area2D
 
+signal on_screen
+
+var velocity = Vector2.ZERO
+var max_velocity = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +13,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	var target = get_tree().get_nodes_in_group("protagonist").back()
+	if target:
+		velocity = target.global_position - global_position
+		velocity = velocity.normalized() * max_velocity
+
+	position += velocity / 60
 	$AnimatedSprite2D.position = position.round() - position
 
 
@@ -18,3 +29,7 @@ func die():
 	$AnimatedSprite2D.play("die")
 	await $AnimatedSprite2D.animation_finished
 	queue_free()
+
+
+func _on_on_screen() -> void:
+	max_velocity = 120.0
