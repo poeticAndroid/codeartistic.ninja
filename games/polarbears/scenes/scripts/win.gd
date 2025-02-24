b78cfde45884d62d9e4d504d69a7593be2c948c1
poeticAndroid <i@poeticAndroid.online>
2025-02-24 10:45:52 +0100

    Remember lowest number of deaths

--- games/polarbears/scenes/scripts/win.gd
+++ games/polarbears/scenes/scripts/win.gd
@@ -4,6 +4,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.persistant.erase("polarbears_bestTime")
+	var deaths = Global.session["polarbears_recordings"].size()-1
+	if deaths < Global.persistant.get_or_add("polarbears_bestDeaths", 1024):
+		Global.persistant["polarbears_bestDeaths"] = deaths
+		%NewRecord.visible = true
+	%YouDied.text = "You died " + str(deaths) + " times."
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
