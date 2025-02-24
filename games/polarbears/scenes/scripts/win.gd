1fe0d2fe42463d937901879d5d9d4c6ff06f8863
poeticAndroid <i@poeticAndroid.online>
2025-02-24 14:22:02 +0100

    changes

--- games/polarbears/scenes/scripts/win.gd
+++ games/polarbears/scenes/scripts/win.gd
@@ -4,8 +4,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.persistant.erase("polarbears_bestTime")
-	var deaths = Global.session["polarbears_recordings"].size()-1
-	if deaths < Global.persistant.get_or_add("polarbears_bestDeaths", 1024):
+	var deaths = randi_range(100, 200)
+	if Global.session.has("polarbears_recordings"):
+		deaths = Global.session["polarbears_recordings"].size()-1
+	if deaths < Global.persistant.get_or_add("polarbears_bestDeaths", deaths):
 		Global.persistant["polarbears_bestDeaths"] = deaths
 		%NewRecord.visible = true
 	%YouDied.text = "You died " + str(deaths) + " times."
