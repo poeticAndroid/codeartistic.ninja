309b581181427d43b99be02fd087d8012c7b7daf
poeticAndroid <i@poeticAndroid.online>
2025-03-31 18:36:25 +0200

    looping world

--- games/restless/scenes/scripts/world.gd
+++ games/restless/scenes/scripts/world.gd
@@ -8,4 +8,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("ui_left"): %Camera.position.x += -4
+	if Input.is_action_pressed("ui_right"): %Camera.position.x += 4
+	if Input.is_action_pressed("ui_up"): %Camera.position.y += -4
+	if Input.is_action_pressed("ui_down"): %Camera.position.y += 4
+	%Camera.position.x = int(%Camera.position.x + 1024) % 1024
+	%Camera.position.y = int(%Camera.position.y + 1024) % 1024
+	%Camera.position = floor(%Camera.position / 4) * 4
