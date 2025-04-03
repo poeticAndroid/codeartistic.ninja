60eb71fec409edd9612a962958fe9c9aa3f0b52a
poeticAndroid <i@poeticAndroid.online>
2025-04-03 11:37:38 +0200

    characters

---
+++ games/restless/scenes/objects/scripts/character.gd
@@ -0,0 +1,64 @@
+extends AnimatedSprite2D
+
+@export var sprite: int:
+	set(val):
+		sprite = val
+		frame = val
+@export var x: float:
+	set(val):
+		position.x = val * 32
+@export var y: float:
+	set(val):
+		position.y = val * 32
+@export var maxsprite: int
+@export var repititionnumber: int
+
+@export var dead: bool
+
+@export var talkedto: bool
+@export var lines = []
+
+var drawnumber: int = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	drawnumber += 1
+	if (drawnumber % 30) == 1:
+		if (randf() * 5) < 2:
+			if not dead: flip_h = not flip_h
+	if name == "animal":
+		if drawnumber % 20 == 1:
+			frame += 1
+			if frame >= maxsprite:
+				repititionnumber -= 1
+				frame = maxsprite - 2
+			if repititionnumber < 1:
+				frame = sprite + randi_range(0, 2)
+				repititionnumber = randi_range(1, 3)
+				moverandomdirection()
+	position = pixel_snap(position)
+
+
+func moverandomdirection():
+	var direction = randi_range(0, 3)
+	if direction < 1:
+		position.x += 1
+	elif direction < 2:
+		position.x -= 1
+	elif direction < 3:
+		position.y -= 1
+	else:
+		position.y += 1
+
+
+func pixel_snap(v: Vector2):
+	v.x = int(v.x + 1024) % 1024
+	v.y = int(v.y + 1024) % 1024
+	v = floor(v / 4) * 4
+	return v
--- games/restless/scenes/scripts/world.gd
+++ games/restless/scenes/scripts/world.gd
@@ -2,10 +2,13 @@ extends Node2D
 
 @export var story: JSON
 
+var current_level = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(story.data)
+	Engine.max_fps = 30
+	show_level(current_level)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +17,30 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"): %Camera.position.x += 4
 	if Input.is_action_pressed("ui_up"): %Camera.position.y += -4
 	if Input.is_action_pressed("ui_down"): %Camera.position.y += 4
-	%Camera.position.x = int(%Camera.position.x + 1024) % 1024
-	%Camera.position.y = int(%Camera.position.y + 1024) % 1024
-	%Camera.position = floor(%Camera.position / 4) * 4
+	%Camera.position = pixel_snap(%Camera.position)
+
+	if Input.is_action_just_pressed("ui_accept"):
+		current_level += 1
+		show_level(current_level)
+
+
+func pixel_snap(v: Vector2):
+	v.x = int(v.x + 1024) % 1024
+	v.y = int(v.y + 1024) % 1024
+	v = floor(v / 4) * 4
+	return v
+
+
+func show_level(lvl: int):
+	if lvl >= story.data.levels.size(): return
+	for char in %Characters.get_children():
+		char.visible = false
+		char.dead = false
+		char.talkedto = false
+		char.flip_h = false
+		var char_data = story.data.levels[lvl].chars
+		if char_data.has(char.name):
+			char_data = char_data[char.name]
+			for prop in char_data:
+				char[prop] = char_data[prop]
+			char.visible = true
